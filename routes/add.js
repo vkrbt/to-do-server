653962f23c6947b3488f5dd960097a99cef6bf36
Vladislav Korbut <v.korbut8@gmail.com>
2017-07-20 15:09:52 +0300

    delete funtion fix

--- routes/add.js
+++ routes/add.js
@@ -3,20 +3,30 @@ var router = express.Router();
 
 var config = require('../config/db');
 var MongoClient = require('mongodb').MongoClient;
+var ObjectId = require('mongodb').ObjectID;
 
 var db;
 
-MongoClient.connect(config.url, function (err, dbase) {
+MongoClient.connect(config.url, function(err, dbase) {
   if (err) {
     throw err;
   }
   db = dbase;
 });
 
-router.post('/', function (req, res) {
+router.post('/', function(req, res) {
   console.log(req.body);
-  const note = {text: req.body.text, active: req.body.active};
-  db.collection('notes').insertOne(note).then(function (data) {
+  let note;
+  if (req.body._id) {
+     note = {
+      text: req.body.text,
+      active: req.body.active,
+      _id: ObjectId(req.body._id),
+    };
+  } else {
+    note = { text: req.body.text, active: req.body.active };
+  }
+  db.collection('notes').insertOne(note).then(function(data) {
     console.log(data.ops);
     res.json(data.ops);
   });
